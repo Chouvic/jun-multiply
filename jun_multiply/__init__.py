e09ddd6f38b94047959cfc4cfeccf1aae581d360
Jun Zhou <jun.zhou@smartia.tech>
2023-11-26 09:22:54 +0000

    Remove double quotes in version

--- jun_multiply/__init__.py
+++ jun_multiply/__init__.py
@@ -1,4 +1,4 @@
 import importlib.metadata
 from jun_multiply.multiplication import Multiplication
 
-__version__ = importlib.metadata.version("__package__")
+__version__ = importlib.metadata.version(__package__)
